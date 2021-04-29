accdf519040b1e0141d2354423cf55ec2d7d1742
cellenar26 <cellenar26@gmail.com>
2021-04-29 16:04:00 +0900

    basic styled-components done

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Redirect, Switch} from "react-router-do
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
+import Header from "./Header";
 
 export default () => (
     <Router>
+        <>
+        <Header />
         <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" component={Search} />
         <Redirect from="*" to="/" />
         </Switch>
+        </>
     </Router>
 )
