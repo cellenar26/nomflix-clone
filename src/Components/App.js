8f883a1d3aa25b43325dceb24f9a2eab1aebe6f3
cellenar26 <cellenar26@gmail.com>
2021-09-20 17:06:55 +0900

    done style css with styled-components, styled-reset

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import GlobalStyles from './GlobalStyles';
+import globalStyles from './GlobalStyles';
 import Header from './Header';
 import AppRouter from "./Router";
 
 const App = () => {
   return (
     <>
-      <Header />
       <AppRouter />
+      <GlobalStyles /> 
     </>
   );
 };
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
-import Home from 'Routes/Home'
-import Search from 'Routes/Search'
-import TV from 'Routes/TV'
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from "Routes/Home";
+import Search from "Routes/Search";
+import TV from "Routes/TV";
+import Header from "./Header";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Switch>
-            <Route  path="/" exact  component={Home}></Route>
-            <Route  path="/tv"  component={TV}></Route>
-            <Route  path="/search"  component={Search}></Route>
-            <Redirect from="*" to="/" />
-            </Switch>
-        </Router>
-    )
-}
-export default AppRouter
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/tv" component={TV}></Route>
+        <Route path="/search" component={Search}></Route>
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  );
+};
+export default AppRouter;
