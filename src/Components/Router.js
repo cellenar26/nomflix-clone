d49d98f49dab139278583346147c42fca8b64221
cellenar26 <cellenar26@gmail.com>
2021-09-21 17:44:22 +0900

    back to BrowserRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,8 +1,8 @@
-import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
-import Detail from "Routes/Detail"
+import Detail from "Routes/Detail";
 import Header from "./Header";
 
 const AppRouter = () => {
@@ -10,9 +10,12 @@ const AppRouter = () => {
     <Router>
       <Header />
       <Switch>
-        <Route path="https://cellenar26.github.io/popcorn-clone/" exact component={Home}></Route>
+        {/* <Route path="https://cellenar26.github.io/popcorn-clone/" exact component={Home}></Route>
         <Route path="https://cellenar26.github.io/popcorn-clone/tv" component={TV}></Route>
-        <Route path="https://cellenar26.github.io/popcorn-clone/search" component={Search}></Route>
+        <Route path="https://cellenar26.github.io/popcorn-clone/search" component={Search}></Route> */}
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/tv" component={TV}></Route>
+        <Route path="/search" component={Search}></Route>
         <Route path="/movie/:id" component={Detail}></Route>
         <Route path="/show/:id" component={Detail}></Route>
         <Redirect from="*" to="/" />
