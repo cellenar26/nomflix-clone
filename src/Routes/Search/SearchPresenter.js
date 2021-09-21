5eadd0e6a629ece0f5908ff327e1428f50555ac6
cellenar26 <cellenar26@gmail.com>
2021-09-21 14:16:22 +0900

    make error component and add

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
+import Message from "Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -43,6 +44,10 @@ const SearchPresenter = ({ movieResults, tvResults, searchTerm, loading, handleS
               ))}
             </Section>
           )}
+          {error && <Message color="#e74c3c" text={error} />}
+          {tvResults && movieResults && tvResults.length === 0 && movieResults.length === 0 && (
+            <Message text="Nothing found" color="#95a5a6" />
+          )}
         </>
       )}
     </Container>
@@ -56,7 +61,7 @@ SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   handleSubmit: PropTypes.func.isRequired,
-  updateTerm: PropTypes.func.isRequired
+  updateTerm: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
