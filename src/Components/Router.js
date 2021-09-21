c9291ba2b99652d65492f01a8b3230f5bbb06d07
cellenar26 <cellenar26@gmail.com>
2021-09-21 17:22:57 +0900

    router modify

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -10,9 +10,9 @@ const AppRouter = () => {
     <Router>
       <Header />
       <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/tv" component={TV}></Route>
-        <Route path="/search" component={Search}></Route>
+        <Route path="https://cellenar26.github.io/popcorn-clone/" exact component={Home}></Route>
+        <Route path="https://cellenar26.github.io/popcorn-clone/tv" component={TV}></Route>
+        <Route path="https://cellenar26.github.io/popcorn-clone/search" component={Search}></Route>
         <Route path="/movie/:id" component={Detail}></Route>
         <Route path="/show/:id" component={Detail}></Route>
         <Redirect from="*" to="/" />
